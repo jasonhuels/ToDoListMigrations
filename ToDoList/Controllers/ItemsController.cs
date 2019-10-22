de7b6f59d198743819fce17625f719c4a659ce34
Student <example@example.com>
2019-10-22 09:22:46 -0700

    add authentication

--- ToDoList/Controllers/ItemsController.cs
+++ ToDoList/Controllers/ItemsController.cs
@@ -4,21 +4,32 @@ using System.Collections.Generic;
 using System.Linq;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+using System.Security.Claims;
 
 namespace ToDoList.Controllers
 {
+  [Authorize] //new line
   public class ItemsController : Controller
   {
     private readonly ToDoListContext _db;
+    private readonly UserManager<ApplicationUser> _userManager; //new line
 
-    public ItemsController(ToDoListContext db)
+    //updated constructor
+    public ItemsController(UserManager<ApplicationUser> userManager, ToDoListContext db)
     {
+      _userManager = userManager;
       _db = db;
     }
 
-    public ActionResult Index()
+    public async Task<ActionResult> Index()
     {
-      return View(_db.Items.ToList());
+        var userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+        var currentUser = await _userManager.FindByIdAsync(userId);
+        var userItems = _db.Items.Where(entry => entry.User.Id == currentUser.Id);
+        return View(userItems);
     }
 
     public ActionResult Create()
@@ -28,8 +39,11 @@ namespace ToDoList.Controllers
     }
 
     [HttpPost]
-    public ActionResult Create(Item item, int CategoryId)
+    public async Task<ActionResult> Create(Item item, int CategoryId)
     {
+        var userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+        var currentUser = await _userManager.FindByIdAsync(userId);
+        item.User = currentUser;
         _db.Items.Add(item);
         if (CategoryId != 0)
         {
@@ -37,7 +51,7 @@ namespace ToDoList.Controllers
         }
         _db.SaveChanges();
         return RedirectToAction("Index");
-}
+    }
 
     public ActionResult Details(int id)
     {
--- ToDoList/Models/CategoryItem.cs
+++ ToDoList/Models/CategoryItem.cs
@@ -1,11 +1,16 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace ToDoList.Models
 {
   public class CategoryItem
     {       
         public int CategoryItemId { get; set; }
+        
         public int ItemId { get; set; }
         public int CategoryId { get; set; }
+        [ForeignKey("ItemId")]
         public Item Item { get; set; }
+        [ForeignKey("CategoryId")]
         public Category Category { get; set; }
     }
 }
